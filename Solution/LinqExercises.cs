5fb236f55063e9713503ab33883d8f4931505d57
T-rav <tmfrisinger@gmail.com>
2018-04-04 15:15:27 +0200

    Working solution

---
+++ Solution/LinqExercises.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution
+{
+    public class LinqExercises
+    {
+        public void NumberTimeFrequency(IEnumerable<int> frequencyNumbers)
+        {
+            var numberFrequenceMatrix = from number in frequencyNumbers
+                group number by number
+                into frequenceyMatrix
+                select new
+                {
+                    Number = frequenceyMatrix.First(),
+                    NumberTimesFrequency = frequenceyMatrix.Count() * frequenceyMatrix.First(),
+                    Frequency = frequenceyMatrix.Count()
+                };
+
+            foreach (var number in numberFrequenceMatrix)
+            {
+                Console.WriteLine($"{number}");
+            }
+        }
+
+        public void NumbersWhoSquareIsGreaterThan20(IEnumerable<int> numbers)
+        {
+            var squares = from number in numbers
+                where number * number > 20
+                select new {Number = number, Square = number * number};
+
+            Console.WriteLine("Numbers who's square is greater than 20:");
+            foreach (var square in squares)
+            {
+                Console.WriteLine($"{square}");
+            }
+        }
+
+        public void NumbersDivisibleBy2(IEnumerable<int> numbers)
+        {
+            var divisibleBy2 = GetNumbersDivisibleBy2(numbers);
+            var print = string.Join(",", divisibleBy2);
+
+            Console.WriteLine($"Numbers divisible by 2 => {print}");
+        }
+
+        private IEnumerable<int> GetNumbersDivisibleBy2(IEnumerable<int> numbers)
+        {
+            var numbersDivisibleBy2 = from number in numbers
+                where number % 2 == 0 && number > 0
+                select number;
+
+            return numbersDivisibleBy2;
+        }
+    }
+}
---
+++ Solution/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exercies = new LinqExercises();
+            Console.WriteLine("For numbers 0-9...");
+            var numbers = Enumerable.Range(0, 10);
+
+            exercies.NumbersDivisibleBy2(numbers);
+            exercies.NumbersWhoSquareIsGreaterThan20(numbers);
+
+            var frequencyNumbers = new int[] {1, 2, 2, 5, 5, 5, 1, 33, 33, 7, 8, 9, 9};
+            var print = string.Join(",", frequencyNumbers);
+
+            Console.WriteLine("");
+            Console.WriteLine($"Numbers and their frequency for {print}...");
+
+            exercies.NumberTimeFrequency(frequencyNumbers);
+
+            Console.ReadLine();
+        }
+    }
+}
